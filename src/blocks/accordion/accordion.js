ae03b1e386b3f28dbdfca486c4b7d2ee0340c064
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2020-01-22 15:49:07 +0300

    accordion > add fix

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -1,10 +1,32 @@
 // accordion
-$(function() {
-	var caption = $('.js-accordion__header'),
-			content = $('.accordion__body', 'js-accordion__body');   
+// $(function() {
+// 	var caption = $('.js-accordion__header'),
+// 			content = $('.accordion__body', 'js-accordion__body');   
 
-	caption.click(function(e) {
-		$(this).toggleClass('active');
-		$(this).next(content).slideToggle(300);
-	});
+// 	caption.click(function(e) {
+// 		$(this).toggleClass('active');
+// 		$(this).next(content).slideToggle(300);
+// 	});
+// });
+
+$(function () {
+	var content = $('.accordion__body', 'js-accordion__body');   
+	var caption = $('.js-accordion__header');
+	
+	caption.on('click', function() {
+	      // закрытие активного пункта при клике на следующий
+	      content.not($(this).next()).slideUp(500);
+	      if ($(this).hasClass('active'))
+	      {
+	      	$(this).removeClass('active');
+	      	$(this).next().slideToggle(400);
+	      }
+	      else
+	      {
+	      	caption.removeClass('active');
+	      	caption.next().slideUp(400);
+	      	$(this).next().slideToggle(400);
+	      	$(this).addClass('active');
+	      }
+	    });
 });
